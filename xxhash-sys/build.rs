12bd42fb90b3171edc93e9ad00037157d5ee29cf
Alex Crichton <alex@alexcrichton.com>
2016-03-15 22:46:36 -0700

    Update submodules on appveyor

--- xxhash-sys/build.rs
+++ xxhash-sys/build.rs
@@ -4,9 +4,10 @@ use std::env;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    cmake::Config::new("xxHash/cmake_unofficial")
-          .cflag("-DXXH_NAMESPACE=__rust_xxhash_sys_")
-          .build();
+    let target = env::var("TARGET").unwrap();
+    let mut cfg = cmake::Config::new("xxHash/cmake_unofficial");
+    cfg.cflag("-DXXH_NAMESPACE=__rust_xxhash_sys_");
+    cfg.build();
     println!("cargo:rustc-link-lib=static=xxhash");
     println!("cargo:rustc-link-search=native={}/lib", out_dir);
     println!("cargo:include=native={}/include", out_dir);
